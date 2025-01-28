f8600aecffc39e4ced07ec7ea3d601ce0db782bb
Ryan <ryanc35@gmail.com>
2025-01-28 11:18:13 -0500

    Minor refactoring for UAA updates
    Minor refactoring updates tied to UAA role changes - utilize main user model reading in main app view vs. component to avoid any possible inconsistencies.

--- Maintenance Application/webapp/controller/App.controller.js
+++ Maintenance Application/webapp/controller/App.controller.js
@@ -8,9 +8,16 @@ sap.ui.define(
       return BaseController.extend("com.at.pd.edi.attr.pdediattr.controller.App", {
 
         onInit: function() {
-          this._controlModel = this.getOwnerComponent().getModel("control"),
-          this._userModel = this.getOwnerComponent().getModel("user");
-          this._controlModel.setProperty("/isAdmin", this._userModel.getProperty("/isAdmin"));
+            const controlModel = this.getOwnerComponent().getModel("control");
+            this._userModel = this.getOwnerComponent().getModel("user");
+            this._pUserDataLoaded ??= this._userModel.dataLoaded();
+            this._pUserDataLoaded.then(function(oPromise){
+              const scopes = this._userModel.getProperty("/scopes"),
+                regex = /^.*\.Admin$/,
+                isAdmin = scopes.filter(item => regex.test(item)).length > 0;
+                controlModel.setProperty("/isAdmin", isAdmin);
+                this._userModel = null;
+            }.bind(this));
         },
 
         onItemSelect: function (oEvent) {
